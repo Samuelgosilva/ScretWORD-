3af512c8813cd17349466ab70a09dffc8d908a1c
Samuelgosilva <samuelesilva@hotmail.com>
2023-11-27 17:01:17 -0300

    Página inicial estilizada

---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css"
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+        <h1>Secret Word</h1>
+        <p>Clique no botão abaixo para inicar o jogo</p>
+        <button>Iniciar jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
