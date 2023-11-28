06052335b3a04c8e377cea3b00a1207b6c208101
Samuelgosilva <samuelesilva@hotmail.com>
2023-11-27 21:43:49 -0300

    alternando estados do jogo

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,11 @@
 import "./StartScreen.css"
 
-const StartScreen = () => {
+const StartScreen = ({startGame}) => {
   return (
     <div className="start">
         <h1>Secret Word</h1>
         <p>Clique no botão abaixo para inicar o jogo</p>
-        <button>Iniciar jogo</button>
+        <button onClick={startGame}>Iniciar jogo</button>
     </div>
   )
 }
